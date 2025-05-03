532a1e5f1b921929c8064ecb1295908663ca2ded
anggunn03 <anggunokt310@gmail.com>
2025-05-03 14:09:13 +0700

    supabase_flutter dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:habit/page/habit_tracker_page.dart'; 
+import 'package:habit/page/habit_tracker_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart'; 
 
-void main() {
+Future<void> main() async {
+  await Supabase.initialize(url: 'https://zyjcucjmurzhnxdrzcbt.supabase.co', anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5amN1Y2ptdXJ6aG54ZHJ6Y2J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyMTk5NTIsImV4cCI6MjA2MDc5NTk1Mn0.s-NdEi8KuQ3wmFXFElSL69yCeQFGFF2goSKGb5ty4EU');
   runApp(const MyApp());
 }
 
