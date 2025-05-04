9b2dcafcc788ccbfc00a96da105fc732fb87b27f
anggunn03 <anggunokt310@gmail.com>
2025-05-04 22:37:57 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:habit/page/habit_tracker_page.dart';
-import 'package:supabase_flutter/supabase_flutter.dart'; 
 
 Future<void> main() async {
   await Supabase.initialize(url: 'https://zyjcucjmurzhnxdrzcbt.supabase.co', anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5amN1Y2ptdXJ6aG54ZHJ6Y2J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyMTk5NTIsImV4cCI6MjA2MDc5NTk1Mn0.s-NdEi8KuQ3wmFXFElSL69yCeQFGFF2goSKGb5ty4EU');
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Habit Tracker',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HabitTrackerPage(),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/login', // mulai dari halaman login
+      routes: {
+        '/habit_tracker': (context) => const HabitTrackerPage(),     
+     },
     );
   }
 }
