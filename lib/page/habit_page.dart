74e421a4aa634ba9e5841efdeb571956884496f3
anggunn03 <anggunokt310@gmail.com>
2025-05-07 19:10:54 +0700

    habit edit page

--- lib/page/habit_page.dart
+++ lib/page/habit_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habit/page/home_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class HabitPage extends StatefulWidget {
@@ -9,7 +10,7 @@ class HabitPage extends StatefulWidget {
 }
 
 class _HabitPageState extends State<HabitPage> {
-  List<dynamic> habit = [];
+  Habit? habit;
   bool initialized = false;
 
   final _formkey = GlobalKey<FormState>();
@@ -20,19 +21,48 @@ class _HabitPageState extends State<HabitPage> {
     if (_formkey.currentState!.validate()) {
       final supabase = Supabase.instance.client;
 
-      if (title.isNotEmpty && description.isNotEmpty) {
+      if (habit != null) {
         await supabase
         .from('habits')
-        .select()
-        .eq('name', title)
-        .order('name');
+        .update({
+          'name': title,
+          'description': description,
+        })
+        .eq('id', habit?.id ?? '');
+        
         
       } else {
-        await supabase.from('habits').update({
+        await supabase.from('habits').insert({
           'name': title,
           'description': description,
         });
       }
+    }  
+  }  
+  Future delete() async {
+    final confirmed = await showDialog<bool>(
+      context: context, 
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Hapus Habit'),
+          content: const Text('Apakah kamu yakin ingin menghapus kebiasaan ini?'),
+          actions: [
+            TextButton(
+            onPressed: () => Navigator.pop(context, false), 
+            child: const Text('Batal'),
+            ),
+            TextButton(
+            onPressed: () => Navigator.pop(context, true),
+            child: const Text('Hapus'),
+            ),
+          ],
+        );
+      },
+    );
+    if (confirmed == true) {
+      final supabase = Supabase.instance.client;
+      await supabase.from('habits').delete().eq('id', habit?.id ?? '');
+      Navigator.pop(context, 'OK');
     }
   }
 
@@ -40,7 +70,41 @@ class _HabitPageState extends State<HabitPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HABIT'),
+        title: Text('${(habit != null) ? 'Edit' : 'Add'} Habit'),
+        actions: [
+          IconButton(icon: const Icon(Icons.delete), 
+          onPressed: delete),
+        ]
+      ),
+      body: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Habit Name'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a habit name';
+                }
+                return null;
+              },
+              onChanged: (value) {
+                setState(() {
+                  title = value;
+                });
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Description'),
+              initialValue: description,
+              onChanged: (value) {
+                setState(() {
+                  description = value;
+                });
+              },
+            ),
+            ElevatedButton(onPressed: save, child: const Text('Save')),
+          ],
+        ) 
       ),
     );
   }
