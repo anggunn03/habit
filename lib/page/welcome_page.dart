33a3ec530616f26df2687a365d560de6d5297c5b
anggunn03 <anggunokt310@gmail.com>
2025-06-17 13:02:53 +0700

    welcomepage

--- lib/page/welcome_page.dart
+++ lib/page/welcome_page.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
+
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(child: Text('Welcome to Login Page')),
+      body: Center(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Selamat Datang di Aplikasi Habit Tracker',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+
+            const SizedBox(height : 20),
+            const Text(
+              'Bangun kebiasaan baik, Raih hidup lebih baik.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height : 40),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/home/page');
+              },
+              child: const Text('Mulai Sekarang'),
+            ),
+          ],
+        )),
     );
   }
 }
