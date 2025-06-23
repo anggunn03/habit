f53e01e89ed0b9c4df13f6cf19b0ff37cd238d28
anggunn03 <anggunokt310@gmail.com>
2025-06-23 14:32:30 +0700

    hapus komen welcome

--- lib/page/welcome_page.dart
+++ lib/page/welcome_page.dart
@@ -1,52 +1,49 @@
 import 'package:flutter/material.dart'; 
 
 
-class WelcomePage extends StatelessWidget { //membuat halaman selamat datang
-  //menggunakan StatelessWidget karena halaman ini tidak akan berubah
+class WelcomePage extends StatelessWidget { 
   const WelcomePage({super.key}); 
 
   @override 
-  Widget build(BuildContext context) { // build untuk membangun widget yang akan ditampilkan
-    //menggunakan Scaffold untuk membuat struktur dasar halaman
+  Widget build(BuildContext context) { 
     return Scaffold( 
-      body: Center( //menggunakan Center untuk menempatkan widget di tengah layar
-        child:Column( //menggunakan Column untuk menampilkan widget secara vertikal
-          mainAxisAlignment: MainAxisAlignment.center, //menggunakan mainAxisAlignment untuk menempatkan widget di tengah secara vertikal
-          children: [ //menggunakan children untuk menampilkan widget yang akan ditampilkan
+      body: Center( 
+        child:Column( 
+          mainAxisAlignment: MainAxisAlignment.center, 
+          children: [ 
             const Text(
-              'Selamat Datang di Aplikasi', //judul aplikasi
-              textAlign: TextAlign.center, //judul ditengah
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold), //menggunakan TextStyle untuk mengatur gaya teks
+              'Selamat Datang di Aplikasi', 
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold), 
             ),
             const Text(
-              'Habit Tracker', //judul aplikasi
-              textAlign: TextAlign.center, //judul ditengah
+              'Habit Tracker', 
+              textAlign: TextAlign.center, 
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
 
-            const SizedBox(height : 20), //jarak antara judul dan deskripsi
+            const SizedBox(height : 20),
             const Text(
               'Bangun kebiasaan baik, Raih hidup lebih baik.',
-              textAlign: TextAlign.center,//menempatkan teks di tengah
+              textAlign: TextAlign.center, 
               style: TextStyle(fontSize: 16), 
             ),
-            const SizedBox(height : 40), //jarak antara deskripsi dan tombol
-            //menggunakan ElevatedButton untuk membuat tombol yang dapat ditekan
+            const SizedBox(height : 40),
             ElevatedButton(
-              style: ElevatedButton.styleFrom(  //menggunakan styleFrom untuk mengatur gaya tombol
+              style: ElevatedButton.styleFrom(  
                 backgroundColor: const Color.fromARGB(255, 33, 82, 243),
                 foregroundColor: Colors.white,
-                shape: RoundedRectangleBorder( //menggunakan RoundedRectangleBorder untuk memberikan bentuk bulat pada tombol
-                  borderRadius: BorderRadius.circular(30), //menggunakan BorderRadius.circular untuk memberikan radius pada sudut tombol
+                shape: RoundedRectangleBorder( 
+                  borderRadius: BorderRadius.circular(30), 
                 ),
-                padding: const EdgeInsets.symmetric(vertical: 16), //menggunakan padding untuk memberikan jarak antara teks dan border tombol
+                padding: const EdgeInsets.symmetric(vertical: 16), 
               ),
               onPressed: () {
-                Navigator.pushNamed(context, '/home/page');  //menggunakan Navigator untuk berpindah ke halaman home ketika tombol ditekan
+                Navigator.pushNamed(context, '/home/page'); 
               },
-              child: const Padding(  //menggunakan Padding untuk memberikan jarak antara teks dan border tombol
-                padding: EdgeInsets.symmetric(horizontal: 24), //menggunakan EdgeInsets.symmetric untuk memberikan jarak horizontal
-                child: Text('Mulai Sekarang'), //teks yang ditampilkan pada tombol
+              child: const Padding(  
+                padding: EdgeInsets.symmetric(horizontal: 24), 
+                child: Text('Mulai Sekarang'),
               )
             ),
           ],
